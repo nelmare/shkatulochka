06c1702cdcc4d2ada5c08f50b3cb52aa1d240ae0
Marina Kuklina <>
2020-12-25 15:07:46 +0300

    Initial commit

---
+++ source/js/burger.js
@@ -0,0 +1,15 @@
+var navMainList = document.querySelector('.main-nav__list');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navMainList.classList.remove('main-nav__list--nojs');
+navToggle.classList.remove('main-nav__toggle--nojs');
+navToggle.classList.add('main-nav__toggle--open');
+navMainList.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', function () {
+  navMainList.classList.toggle('main-nav--closed');
+  navMainList.classList.toggle('main-nav--open');
+  navToggle.classList.toggle('main-nav__toggle--open');
+  navToggle.classList.toggle('main-nav__toggle--closed');
+  document.body.classList.toggle('no-scroll');
+});
